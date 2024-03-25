95829f788cf5adae1a59c740e5af742d3bb57f00
blakley <blakley.ent@gmail.com>
2024-03-25 18:46:13 -0500

    Added model creation & training config

--- src/scripts/model.py
+++ src/scripts/model.py
@@ -5,12 +5,12 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 import tensorflow as tf
 from tensorflow import keras
+from tensorflow.keras import layers, models
 
 # Captcha Classification Model
 class Captcha():
 	def __init__(self):
-		# 
-		self.setup()
+		pass
 
 
 	'''
@@ -33,14 +33,70 @@ class Captcha():
 		self.classes = self.train.class_names
 		# print(self.classes)
 
+		# define RGB image sizes
+		self.shape = (128, 128, 3)
+		
+		# create model next
+		self.create()
 
 	'''
 		================================
 		
 		================================
 	'''	
-	def function(self):
-		pass
+	def create(self):
+		# create sequential model
+		self._model = models.Sequential()
+
+		# add input layer
+		self._model.add(layers.Input(shape = self.shape))
+
+		# add convolutional layers
+		self._model.add(layers.Conv2D(32, kernel_size=(3, 3), activation='relu'))
+		self._model.add(layers.MaxPooling2D(pool_size=(2, 2)))
+		
+		self._model.add(layers.Conv2D(64, kernel_size=(3, 3), activation='relu'))
+		self._model.add(layers.MaxPooling2D(pool_size=(2, 2)))
+		
+		self._model.add(layers.Conv2D(128, kernel_size=(3, 3), activation='relu'))
+		self._model.add(layers.MaxPooling2D(pool_size=(2, 2)))
+
+		# flatten layer
+		self._model.add(layers.Flatten())
+
+		# add dense layers : softmax activation for multi-class classification
+		self._model.add(layers.Dense(128, activation='relu'))
+		self._model.add(layers.Dense(len(self.classes), activation='softmax'))
+
+		# compile model next
+		self.compile()
+
+	'''
+		================================
+		
+		================================
+	'''	
+	def compile(self):
+		model.compile(
+			optimizer='adam', 
+			loss='sparse_categorical_crossentropy', 
+			metrics=['accuracy']
+		)
+
+		# train model next
+		self.train()
+
+	'''
+		================================
+		
+		================================
+	'''	
+	def train(self):
+		self.history = model.fit(
+			self.train, 
+			validation_data = self.validation, 
+			epochs = 10
+		)
 
 
 	'''
@@ -48,19 +104,32 @@ class Captcha():
 		
 		================================
 	'''	
-	def function(self):
+	def statistics(self):
 		pass
-
 
 	'''
 		================================
 		
 		================================
 	'''	
-	def function(self):
+	def save(self):
 		pass
+
+	'''
+		================================
+		
+		================================
+	'''	
+	def load(self):
+		pass
+
 
 # 
 if __name__ == '__main__':
 	# model instance
 	_model = Captcha()
+
+	# model setup outline
+	_model.setup()
+
+	# 
