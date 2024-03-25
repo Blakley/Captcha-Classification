66ecb6ba8adb3feda6ca1445657223981b240d06
blakley <blakley.ent@gmail.com>
2024-03-25 18:11:10 -0500

    Created Classification Model Class Structure

---
+++ src/scripts/model.py
@@ -0,0 +1,53 @@
+import os
+
+# suppress tensorflow messages
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+import tensorflow as tf
+from tensorflow import keras
+
+# Captcha Classification Model
+class Captcha():
+	def __init__(self):
+		pass
+
+
+	'''
+		================================
+
+		================================
+	'''	
+	def function():
+		pass
+
+
+	'''
+		================================
+		
+		================================
+	'''	
+	def function():
+		pass
+
+
+	'''
+		================================
+		
+		================================
+	'''	
+	def function():
+		pass
+
+
+	'''
+		================================
+		
+		================================
+	'''	
+	def function():
+		pass
+
+# 
+if __name__ == '__main__':
+	# model instance
+	_model = Captcha()
--- src/scripts/test.py
+++
@@ -1,4 +0,0 @@
-
-# dataset: https://www.kaggle.com/datasets/aneeshtickoo/hcaptcha-dataset
-def test():
-    print("hello")
